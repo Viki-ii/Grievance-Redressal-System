5615eb9d228c334a31dc5a3cbd4193642032cacd
Vedalaskshmi B S <vedalakshmi05@gmail.com>
2026-09-19 21:16:35 +0530

    Add staff middleware

---
+++ app/Http/Middleware/StaffMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class StaffMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!auth()->check()) {
+            return redirect('/login');
+        }
+
+        if (auth()->user()->role !== 'staff') {
+            abort(403);
+        }
+
+        return $next($request);
+    }
+}
